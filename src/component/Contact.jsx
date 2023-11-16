c5007600b506a3f640ceddc88b259e91a94e64c9
dityalif <ditya818@gmail.com>
2023-11-16 23:40:53 +0700

    looks update

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -2,21 +2,23 @@ import { useState } from 'react'
 import ContactCard from './ContactCard'
 import LinkedIn from '../../public/assets/linkedin.png'
 import Instagram from '../../public/assets/instagram.png'
+import Github from '../../public/assets/github.png'
 
 const data=[
   {id: 1, name: 'LinkedIn', username: 'Muhammad Raditya Alif Nugroho', img:LinkedIn, link: 'https://www.linkedin.com/in/radityalif'},
-  {id: 2, name: 'Instagram', username: 'dityalif', img:Instagram, link: 'https://www.instagram.com/dityalif/'}
+  {id: 2, name: 'Instagram', username: 'dityalif', img:Instagram, link: 'https://www.instagram.com/dityalif/'},
+  {id: 3, name: 'GitHub', username: 'dityalif', img:Github, link: 'https://github.com/dityalif'}, 
 ];
 
 const Contact = () => {
   const [contacts, setContacts] = useState();
   return (
-    <div className="my-20 min-h-screen">
+    <div className="m-20 min-h-screen">
         <h1 className="text-center font-bold text-4xl py-5">You can Contact Me Here</h1>
-        <div className='flex place-content-center'>
-        {data.map((data) => 
-          <ContactCard key={data.id} name={data.name} username={data.username} img={data.img} link={data.link}/>
-        )}
+        <div className='w-3/4 align-center mx-auto grid gap-5 grid-cols-1 md:grid-cols-3'>
+          {data.map((data) => 
+            <ContactCard key={data.id} name={data.name} username={data.username} img={data.img} link={data.link}/>
+          )}
         </div>
     </div>
   )
--- src/component/ContactCard.jsx
+++ src/component/ContactCard.jsx
@@ -1,15 +1,20 @@
 const ContactCard = (props) => {
   return (
     <div>
-            <a href={props.link} className='block m-5'>
-                <div key={props.id} className='flex w-96 bg-gradient-to-r from-[#00587A] to-[#0F3057] p-5 align-center items-center text-white mx-5 rounded-2xl shadow-2xl transition duration-200 hover:scale-105'>
+          <a href={props.link} className='my-5 flex item-center'>
+              <div key={props.id} className='flex w-96 justify-between bg-gradient-to-r from-[#00587A] to-[#0F3057] p-5 align-center items-center text-white mx-auto rounded-2xl shadow-2xl transition duration-200 hover:scale-105'>
+                  <div className="flex align-center items-center">  
                     <img src={props.img} className='w-11 h-11 mr-5'/>
                     <div className='flex flex-col'>  
                         <h1 className='text-2xl font-bold'>{props.name}</h1>
                         <p>{props.username}</p>
                     </div>
-                </div>
-            </a>
+                  </div>
+                  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2 place-items-end" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+              </div>
+          </a>
     </div>
   )
 }
